0508252da7d01bd29f0c4a6c4c1e5369ec1a46a7
unknown <dgoodenough@proton.me>
2022-10-29 08:19:27 -0700

    First Commit, add event listener to the post button

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// your code here
+let postDiv = document.querySelector('.posts')
+
+let btn = document.querySelector('#submit');
+
+btn.addEventListener("click", () => {
+  let name = document.querySelector('#name').value;
+  let message = document.querySelector('#message').value;
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let time = `${hours}:${minutes}:${seconds}`
+
+  postP = document.createElement('p');
+  postText = document.createTextNode(`${message} - posted by ${name} @ ${time}`);
+
+  postP.appendChild(postText);
+  postDiv.appendChild(postP)
+})
